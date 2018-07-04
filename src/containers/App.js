31cff481ae9864488a8ba35b72beef2cefbccbb6
Clifton Barnes <clifton.barnes@rovercode.com>
2018-07-04 19:19:39 -0400

    Call API on main page to validate JWT token from login

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,61 @@
-import React from 'react';
-import { Header, Segment } from 'semantic-ui-react';
+import React, { Component, Fragment } from 'react';
+import { Header, Loader } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withCookies, Cookies } from 'react-cookie';
 
-const App = () => (
-  <Segment>
-    <Header>
-      Welcome to rovercode!
-    </Header>
-  </Segment>
-);
+import axios from 'axios';
 
-export default App;
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rovers: null,
+    };
+  }
+
+  componentDidMount() {
+    const { cookies } = this.props;
+
+    return axios.get('/api/v1/rovers/', {
+      headers: {
+        Authorization: `JWT ${cookies.get('auth_jwt')}`,
+      },
+    })
+      .then((response) => {
+        this.setState({
+          rovers: response.data,
+        });
+      })
+      .catch(() => {
+        this.setState({
+          rovers: [],
+        });
+      });
+  }
+
+  render() {
+    const { rovers } = this.state;
+
+    return (
+      <Fragment>
+        {
+           rovers !== null ? (
+             <Header>
+               {`Welcome to rovercode, you have ${rovers.length} rover(s)!`}
+             </Header>
+           ) : (
+             <Loader active />
+           )
+        }
+      </Fragment>
+    );
+  }
+}
+
+App.propTypes = {
+  cookies: PropTypes.instanceOf(Cookies).isRequired,
+};
+
+export default withCookies(App);
