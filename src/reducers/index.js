813bf6cc3614d0e328a006fc61a45615394cfee6
Clifton Barnes <clifton.barnes@rovercode.com>
2018-09-27 21:34:55 -0400

    Add sensor state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import code from './code';
 import console from './console';
 import rover from './rover';
+import sensor from './sensor';
 
 export default combineReducers({
   code,
   console,
   rover,
+  sensor,
 });
