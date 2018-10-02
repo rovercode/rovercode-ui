830db2b43822db29f83faba01dff78f978d929e1
Clifton Barnes <clifton.barnes@rovercode.com>
2018-10-01 22:29:03 -0400

    Add CodeViewer component

---
+++ src/components/__tests__/CodeViewer.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
+import configureStore from 'redux-mock-store';
+
+import CodeViewer from '../CodeViewer';
+
+describe('The CodeViewer component', () => {
+  const mockStore = configureStore();
+
+  test('renders on the page with no errors', () => {
+    const store = mockStore({
+      code: {
+        jsCode: 'test code',
+      },
+    });
+    const wrapper = mount(<CodeViewer store={store} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  test('handles blank code with no errors', () => {
+    const store = mockStore({
+      code: {
+        jsCode: null,
+      },
+    });
+    const wrapper = mount(<CodeViewer store={store} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
