a97b7927c5de4c59346c4932a7a91173a9e352fd
brymon68 <brycemontano1@gmail.com>
2018-10-14 17:38:06 -0600

    working on chatform action test

--- src/actions/__test__/chatform.js
+++ src/actions/__test__/chatform.js
@@ -2,6 +2,7 @@ import {categorySelectChange,
   bodyInputChange, 
   subjectInputChange, 
   experienceSelectChange,
+  categorySelectChange,
   WHAT_NEXT, FUN_IDEAS, DEBUG, 
   EXPERT, PRETTY_NEW, FIRST_TIME,FOR_A_WHILE, 
   CATEGORY_SELECT_CHANGE, 
@@ -10,13 +11,23 @@ import {categorySelectChange,
   SUBJECT_INPUT_CHANGE}
   from '../chatform'
   
-  describe('Category Select Action', () => {
-    test('categorySelectChange', () => {
-      const action = categorySelectChange(DEBUG);
-      const { type, payload } = action;
-  
-      expect(type).toEqual('SELECT_CHANGE');
-      expect(payload).toEqual(COVERED);
-    });
+describe('Category Select Action - Debug', () => {
+  test('categorySelectChange', () => {
+    const action = categorySelectChange(DEBUG);
+    const { type, payload } = action;
+
+    expect(type).toEqual('DEBUG');
+    expect(payload).toEqual(DEBUG);
   });
+});
+  
+describe('Category Select Action - Fun Ideas', () => {
+  test('categorySelectChange', () => {
+    const action = categorySelectChange(FUN_IDEAS);
+    const { type, payload } = action;
+
+    expect(type).toEqual('FUN_IDEAS');
+    expect(payload).toEqual(FUN_IDEAS);
+  });
+});
   
