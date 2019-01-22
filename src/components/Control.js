ef3658e191823cf293403a7533b8d205eb15ec28
Clifton Barnes <clifton.barnes@rovercode.com>
2019-01-21 20:42:52 -0500

    Animated control buttons with icon and text

--- src/components/Control.js
+++ src/components/Control.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
-import { Button } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 import { hot } from 'react-hot-loader';
 import PropTypes from 'prop-types';
 
@@ -21,19 +21,39 @@ const Control = ({ code, changeExecutionState }) => (
   <Fragment>
     {
       code.execution === EXECUTION_RUN ? (
-        <Button onClick={() => changeExecutionState(EXECUTION_STOP)}>
-          Stop
+        <Button onClick={() => changeExecutionState(EXECUTION_STOP)} animated="vertical">
+          <Button.Content hidden>
+            Stop
+          </Button.Content>
+          <Button.Content visible>
+            <Icon name="stop" />
+          </Button.Content>
         </Button>
       ) : (
         <Fragment>
-          <Button onClick={() => changeExecutionState(EXECUTION_STEP)}>
-            Step
+          <Button onClick={() => changeExecutionState(EXECUTION_STEP)} animated="vertical">
+            <Button.Content hidden>
+              Step
+            </Button.Content>
+            <Button.Content visible>
+              <Icon name="step forward" />
+            </Button.Content>
           </Button>
-          <Button onClick={() => changeExecutionState(EXECUTION_RUN)}>
-            Run
+          <Button onClick={() => changeExecutionState(EXECUTION_RUN)} animated="vertical">
+            <Button.Content hidden>
+              Run
+            </Button.Content>
+            <Button.Content visible>
+              <Icon name="play" />
+            </Button.Content>
           </Button>
-          <Button onClick={() => changeExecutionState(EXECUTION_RESET)}>
-            Reset
+          <Button onClick={() => changeExecutionState(EXECUTION_RESET)} animated="vertical">
+            <Button.Content hidden>
+              Reset
+            </Button.Content>
+            <Button.Content visible>
+              <Icon name="repeat" />
+            </Button.Content>
           </Button>
         </Fragment>
       )
