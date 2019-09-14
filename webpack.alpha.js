2d1f77e99f44015c5d32e8ea33c871c6bb5259cd
Clifton Barnes <clifton.barnes@rovercode.com>
2019-09-14 15:03:58 -0400

    Move mode into configuration files

--- webpack.alpha.js
+++ webpack.alpha.js
@@ -2,6 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  mode: 'development',
   devServer: {
     hot: true,
     historyApiFallback: true,
