a996eb3b25134723b913a01a78c93eb0c31b55c4
Brady L. Hurlburt <brady.hurlburt@rovercode.com>
2020-04-29 00:09:04 -0400

    Set up semantic-ui-less

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-less/semantic.less';
 
 import React from 'react';
 import { render } from 'react-dom';
--- webpack.common.js
+++ webpack.common.js
@@ -33,6 +33,13 @@ module.exports = {
         }),
       },
       {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader']
+        }),
+      },
+      {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           {
