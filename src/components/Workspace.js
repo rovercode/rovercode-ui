d14cede36cac15fcb4be77139ed12a5fa0eae953
Brady Hurlburt <brady.hurlburt@rovercode.com>
2020-06-15 15:24:22 -0400

    Remove commented code

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -322,7 +322,6 @@ class Workspace extends Component {
     });
 
     workspace.addChangeListener(
-      // this.updateCode
       debounce(
         parseInt(SAVE_DEBOUNCE_TIME, 10) || 5000, this.updateCode, // eslint-disable-line no-undef
       ),
