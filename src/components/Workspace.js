d5bf016f204b5c02f324a989ce44cdc2ab56e701
Clifton Barnes <clifton.barnes@rovercode.com>
2020-08-27 20:46:38 -0400

    Add error message when saving fails

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -21,6 +21,7 @@ import {
   EXECUTION_RESET,
 } from '@/actions/code';
 import { append, clear } from '@/actions/console';
+import { showNotification } from '@/actions/notification';
 import { send } from '@/actions/rover';
 import { COVERED } from '@/actions/sensor';
 import BlocklyApi from '@/utils/blockly-api';
@@ -34,9 +35,17 @@ const mapDispatchToProps = (dispatch, { cookies }) => ({
   clearConsole: () => dispatch(clear()),
   updateXmlCode: (xmlCode) => dispatch(actionUpdateXmlCode(xmlCode)),
   sendToRover: (channel, message) => dispatch(send(channel, message)),
-  saveProgram: (id, content, name, lessonId) => dispatch(
+  saveProgram: (id, content, name, lessonId, errorMessage) => dispatch(
     actionSaveProgram(id, content, name, lessonId, authHeader(cookies)),
-  ).catch(checkAuthError(dispatch)),
+  ).catch(checkAuthError(dispatch)).catch((error) => {
+    const serverError = error.response && [502, 503, 504].includes(error.response.status);
+    if (error.message === 'Network Error' || serverError) {
+      // There was an error contacting the API
+      dispatch(showNotification(errorMessage, 4000, 'error'));
+    } else {
+      throw error;
+    }
+  }),
 });
 
 const toolbox = `
@@ -178,7 +187,7 @@ const toolbox = `
 class Workspace extends Component {
   constructor(props) {
     super(props);
-    const { writeToConsole } = this.props;
+    const { intl, writeToConsole } = this.props;
 
     this.sensorStateCache = [];
     this.sensorStateCache.A_BUTTON = false;
@@ -191,6 +200,11 @@ class Workspace extends Component {
     this.runningEnabled = false;
     this.highlightPause = false;
     this.interpreter = null;
+    this.saveErrorMessage = intl.formatMessage({
+      id: 'app.workspace.save_error',
+      description: 'Indicates that saving the program has failed',
+      defaultMessage: 'Error saving program',
+    });
 
     this.api = new BlocklyApi(this.highlightBlock, this.beginSleep,
       this.sensorStateCache, writeToConsole, this.sendToRover);
@@ -385,7 +399,7 @@ class Workspace extends Component {
     const xmlCode = this.updateXmlCode();
 
     if (!code.isReadOnly && code.id) {
-      saveProgram(code.id, xmlCode, code.name, code.lesson);
+      saveProgram(code.id, xmlCode, code.name, code.lesson, this.saveErrorMessage);
     }
   }
 
