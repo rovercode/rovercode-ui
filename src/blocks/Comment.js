acc932cdb3f63151e355c7470b6488fb3844e2b6
Brady L. Hurlburt <brady.hurlburt@rovercode.com>
2021-07-24 22:41:20 -0400

    Add comment block

---
+++ src/blocks/Comment.js
@@ -0,0 +1,26 @@
+import Blockly from 'blockly';
+import { display } from './Color';
+
+const Comment = {
+  definition: {
+    init() {
+      this.appendDummyInput()
+        .appendField('/* ')
+        .appendField(new Blockly.FieldTextInput('Write a comment here!'), 'COMMENT')
+        .appendField('*/');
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(display.HUE);
+      this.setTooltip('Write a comment to explain your code. It will not affect how your program runs.');
+      this.setHelpUrl('https://docs.rovercode.com/blocks/comment');
+    },
+  },
+  generator: (block) => {
+    const valueComment = block.getFieldValue('COMMENT');
+    const code = `/* ${valueComment} */\n`; // Injection risk, but more power to them
+    return code;
+  },
+};
+
+export default Comment;
